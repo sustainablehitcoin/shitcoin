330415f6382b9f9dd7fbe7dbc945784292f73eac
Teddy Fluffster <fluffyteddybear1910@gmail.com>
2018-01-19 11:38:30 +0000

    Timer working.

---
+++ src/ICOCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Timer from './Timer.jsx'
+
+const ICOCard = () => {
+    
+    return (
+        <div id="token-sale" >
+            <div className="card-container">
+                <div className="card-info">
+                    <div className="timer">
+                        <Timer />
+                    </div>
+                </div>
+                <div></div>
+
+            </div>
+            <div></div>
+        </div>
+        
+    )
+}
+
+export default ICOCard
